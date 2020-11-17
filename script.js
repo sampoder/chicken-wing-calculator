a83c02a2ac531ae9fafcf8c57f0fcb84f8985930
radiantly <44368997+radiantly@users.noreply.github.com>
2020-11-17 23:40:55 +0530

    commit changes by everybody

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-function calcNumbers(result){
-  form.displayResult.value=form.displayResult.value+result;
+const dp = document.getElementById("displayResult")
+
+const calculate = () => {
+  if (!dp.value) return "";
+  try {
+    return eval(dp.value);
+  } catch (ex) {
+    console.error(ex);
+    return "";
+  }
 }
+
+const operators = ["+", "-", "*", "/", "."];
+
+// this binds the onclick event listener to all buttons instead of doing it manually for each button over at index.html
+document.querySelectorAll("button").forEach(elem => {
+  elem.addEventListener("click", e => {
+    let current = dp.value;
+    let val = elem.value;
+
+    // do not allow two operators in a row
+    if (dp.value && operators.includes(dp.value.slice(-1)) && operators.includes(val))
+      current = current.slice(0, -1); // remove previous operator
+
+    dp.value = val == "=" ? calculate() : current + val;
+  });
+});
+
+// handling submit allows us to catch the Enter key
+document.getElementById("form").addEventListener("submit",  e => {
+  e.preventDefault();
+  console.log("hit")
+  dp.value = calculate();
+});
